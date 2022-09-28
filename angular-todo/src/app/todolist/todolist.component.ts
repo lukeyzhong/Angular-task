d97e2e576c3b441d8d9e29942720c718654d00c7
lukeyzhong <lukeyzhong@gmail.com>
2022-09-28 09:11:01 -0500

    update

--- angular-todo/src/app/todolist/todolist.component.ts
+++ angular-todo/src/app/todolist/todolist.component.ts
@@ -5,36 +5,33 @@ import { TodoService } from '../todo.service';
 @Component({
   selector: 'app-todolist',
   templateUrl: './todolist.component.html',
-  styleUrls: ['./todolist.component.scss']
+  styleUrls: ['./todolist.component.scss'],
 })
 export class TodolistComponent implements OnInit {
   todolist: Todo[] = [];
   inputTodo: string = '';
 
-  constructor(private todoService: TodoService) { }
+  constructor(private todoService: TodoService) {}
 
   ngOnInit(): void {
-    this.todoService.getTodos().subscribe((data)=>{
+    this.todoService.getTodos().subscribe((data) => {
       this.todolist = data;
-    })
+    });
   }
 
-  deletetodo(id: string){
+  deletetodo(id: string) {
     this.todolist = this.todolist.filter((ele: any) => +ele.id !== +id);
     this.todoService.deleteTodo(id).subscribe();
-  } 
+  }
 
   addtodo(input: string) {
     const newtodo: any = {
       userId: 101,
       title: input,
     };
-
     this.todoService.addTodo(newtodo).subscribe((data) => {
       this.todolist = [data, ...this.todolist];
     });
-
     this.inputTodo = '';
   }
-
 }
