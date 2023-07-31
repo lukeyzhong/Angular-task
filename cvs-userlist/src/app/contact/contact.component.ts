687d1aa89fdb387e543f1ad3f954ca463442d98f
lukeyzhong <lukeyzhong@gmail.com>
2023-07-31 12:51:20 -0500

    update

--- cvs-userlist/src/app/contact/contact.component.ts
+++ cvs-userlist/src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
+  selected = false;
+
+  contactForm = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    message: new FormControl('', [Validators.required, Validators.minLength(10)]),
+  })
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    console.log(this.contactForm.value);
+    this.selected = true;
+
+  }
+
+  get name() {
+    return this.contactForm.get('name')
+  }
+
+  get message() {
+    return this.contactForm.get('message')
   }
 
 }
